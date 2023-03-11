7d2317b98d382cdf726da21884efad4ca4dad38e
Kushal Kumar <kushalkumargupta4@gmail.com>
2023-03-12 00:57:23 +0530

    feat: fetch miners and sharders
    Signed-off-by: Kushal Kumar <kushalkumargupta4@gmail.com>

--- cmd/setup-mpks.go
+++ cmd/setup-mpks.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"onboarding-cli/types"
+	"onboarding-cli/util"
 )
 
 var setupMPKS = &cobra.Command{
@@ -11,7 +13,21 @@ var setupMPKS = &cobra.Command{
 	Short: "Generates mpks and shares and sends them",
 	Long:  "Generates the mpks, and responsible for sharding and sending them",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Setup MPKS called")
+
+		getReq, err := util.NewHTTPGetRequest("http://localhost:3000/nodes")
+		if err != nil {
+			panic(err)
+		}
+		getResponse, err := getReq.Get()
+		respBody := getResponse.PostResponse.Body
+		var nodes types.Nodes
+		err = json.Unmarshal([]byte(respBody), &nodes)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(nodes)
+
 	},
 }
 
